1890d59e15251ad8ac3b488aac6850de6e4809b9
simonheglasik <simonheglasik@gmail.com>
2020-01-09 08:58:41 +0100

    Add project files.

---
+++ GPSTEST/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Geolocator;
+
+namespace GPSTEST
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 50;
+
+            var positon = await locator.GetPositionAsync();
+
+            LongitudeLabel.Text = positon.Longitude.ToString();
+            LatitudeLabel.Text = positon.Latitude.ToString();                                                                                                                                                                             
+        }
+    }
+}
